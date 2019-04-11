983bbc1044509a15ce9e785e0bc43a172d33ce44
Jessica Hong <jessicahong.24@gmail.com>
2019-04-11 20:48:33 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -2,6 +2,7 @@ def prime?(number)
   are_prime =[2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89,97]
 
    are_prime.each do |x|
+<<<<<<< HEAD
 
               if number <= 1
                 return false
@@ -24,3 +25,22 @@ def prime?(number)
     
 end
 
+=======
+      if number <= 1
+        return false
+      elsif number == x
+        return true
+      elsif number % 2 == 0
+        return false
+      elsif number % x == 0
+        return false
+      elsif number 
+        return false
+      else 
+        return true
+      
+      end
+    end
+    
+end
+>>>>>>> 316decdcf5c128bddea3822f44a5dbb27db7940b
